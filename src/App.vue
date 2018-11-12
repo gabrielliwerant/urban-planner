9f15b0a09ff6dba016c3416c604de8fb0b2b96dd
Gabriel Liwerant <gabriel@abrightconcept.com>
2018-11-11 23:03:02 -0500

    Add call to the Bored API and display data

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <h1>{{ message }}</h1>
+    <p>{{ activity }}</p>
   </div>
 </template>
 
@@ -9,7 +10,27 @@
     name: 'app',
     data () {
       return {
-        message: 'Hello World!'
+        message: 'Urban Planner',
+        activity: {}
+      }
+    },
+    created: function() {
+      this.get();
+    },
+    methods: {
+      get: function() {
+        let vm = this;
+
+        fetch('http://www.boredapi.com/api/activity/')
+          .then(res => {
+            if (res.ok) return res.json().then(body => vm.activity = body);
+
+            throw new Error('Got a not-ok response.');
+          })
+          .catch(function(error) {
+            console.log('Fetch data has failed: ', error.message);
+            vm.activity = 'Error fetching data.';
+          });
       }
     }
   }
