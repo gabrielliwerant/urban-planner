4030b0ed074aa947b0ff84dbcd418e062ade4c1b
Gabriel Liwerant <gabriel@abrightconcept.com>
2018-11-11 23:52:02 -0500

    Add form for request input and refactor activities to array/list

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   <div id="app">
     <h1>{{ message }}</h1>
-    <p>{{ activity }}</p>
+
+    <form v-on:submit.prevent="onSubmit">
+      <input v-model="dayNum" placeholder="Number of days to plan...">
+      <input type="submit">
+      <div>{{ formError }}</div>
+      <div>{{ formSuccess }}</div>
+    </form>
+
+    <ol>
+      <li v-for="activity in activities">
+        {{ activity.activity }}
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -11,19 +23,42 @@
     data () {
       return {
         message: 'Urban Planner',
-        activity: {}
+        dayNum: '',
+        formError: '',
+        formSuccess: '',
+        activities: []
       }
     },
-    created: function() {
-      this.get();
-    },
     methods: {
+      onSubmit: function() {
+        this.activities = [];
+        this.dayNum = parseInt(this.dayNum, 10);
+
+        if (
+          this.dayNum < 0 ||
+          this.dayNum > 31 ||
+          isNaN(this.dayNum) ||
+          typeof this.dayNum !== 'number'
+        ) {
+          this.formSuccess = '';
+          this.formError = 'Please enter a number between 0 and 31';
+          this.dayNum = '';
+        } else {
+          this.formError = '';
+          this.formSuccess = 'Form successfully submitted!';
+
+          // Make all of our requests in a row
+          for (var i = 0; i < this.dayNum; i += 1) {
+            this.get();
+          }
+        }
+      },
       get: function() {
         let vm = this;
 
         fetch('http://www.boredapi.com/api/activity/')
           .then(res => {
-            if (res.ok) return res.json().then(body => vm.activity = body);
+            if (res.ok) return res.json().then(body => vm.activities.push(body));
 
             throw new Error('Got a not-ok response.');
           })
